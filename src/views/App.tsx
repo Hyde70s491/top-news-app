02c61286eb61addce76637575ecc2ae337180432
Stefan Misic <stefan.misic.491@gmail.com>
2021-01-13 14:43:58 +0100

    Remove unnecessary code; Create base project architecture; Add base styling

---
+++ src/views/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.scss';
+import '../stylesheets/main.scss';
+
+function App() {
+  return (
+    <div className="app">
+      Hello, Top News App!
+    </div>
+  );
+}
+
+export default App;
