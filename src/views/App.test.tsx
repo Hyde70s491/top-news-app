8a52f8fa4ec0dd2b03f8976ab937102161f2ea59
Stefan Misic <stefan.misic.491@gmail.com>
2021-01-19 16:05:27 +0100

    Add base test

--- src/views/App.test.tsx
+++ src/views/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("<App /> test", () => {
+  it("renders welcome message", () => {
+    render(<App />);
+
+    const welcomeMessage = screen.getByText(/Hello, Top News App!/i);
+
+    expect(welcomeMessage).toBeInTheDocument();
+  });
 });
