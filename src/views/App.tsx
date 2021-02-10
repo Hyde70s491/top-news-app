e204d4e7ffd4ef1791cba469433cee48d6cfcf7c
Stefan Misic <stefan.misic.491@gmail.com>
2021-02-10 15:19:37 +0100

    Add AppHeader component

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,21 +1,14 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Route, Switch } from "react-router-dom";
 
+import AppHeader from "./layout/app-header/AppHeader";
 import "./App.scss";
 import "../stylesheets/main.scss";
 
-import { selectWelcomeMessage } from "../stores/rootReducer";
-
 const App: React.FC = () => {
-  const welcomeMessage: string = useSelector(selectWelcomeMessage);
-
   return (
-    <Switch>
-      <Route exact path="/">
-        <div className="app">{welcomeMessage}</div>
-      </Route>
-    </Switch>
+    <div className="app">
+      <AppHeader></AppHeader>
+    </div>
   );
 };
 
---
+++ src/views/layout/app-header/AppHeader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./AppHeader.scss";
+
+const AppHeader: React.FC = () => {
+  return (
+    <div className="app-header">
+      <div className="app-header__content"></div>
+    </div>
+  );
+};
+
+export default AppHeader;
