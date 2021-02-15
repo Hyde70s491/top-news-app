0a2523c96342d4ccf07030025c287eabfb64dfa7
Stefan Misic <stefan.misic.491@gmail.com>
2021-02-15 12:24:03 +0100

    Add CategoryLinks to AppFooter

--- src/views/components/category-links/CategoryLinks.tsx
+++ src/views/components/category-links/CategoryLinks.tsx
@@ -3,9 +3,19 @@ import { NavLink } from "react-router-dom";
 
 import "./CategoryLinks.scss";
 
-const CategoryLinks: React.FC = () => {
+interface CategoryLinksProps {
+  readonly linksClassName?: string;
+}
+
+const CategoryLinks: React.FC<CategoryLinksProps> = (
+  props: React.PropsWithChildren<CategoryLinksProps>
+) => {
+  const { linksClassName } = props;
+
   return (
-    <div className="category-links">
+    <div
+      className={`category-links${linksClassName ? " " + linksClassName : ""}`}
+    >
       <NavLink
         className={`category-links__entity${false ? " frozen-link" : ""}`}
         activeClassName="category-links__entity--active"
--- src/views/layout/app-footer/AppFooter.tsx
+++ src/views/layout/app-footer/AppFooter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import CategoryLinks from "../../components/category-links/CategoryLinks";
 import SearchBar from "../../components/search-bar/SearchBar";
 import "./AppFooter.scss";
 
@@ -8,6 +9,7 @@ const AppFooter: React.FC = () => {
     <div className="app-footer">
       <div className="app-footer__content">
         <SearchBar barClassName="search-bar--footer" />
+        <CategoryLinks linksClassName="category-links--footer" />
       </div>
     </div>
   );
