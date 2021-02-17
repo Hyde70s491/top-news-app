6992ae60efd553b244ffa2bce703322396658c90
Stefan Misic <stefan.misic.491@gmail.com>
2021-02-17 15:44:30 +0100

    Add TopNews grid layout

--- src/views/pages/home/top-news/TopNews.tsx
+++ src/views/pages/home/top-news/TopNews.tsx
@@ -6,7 +6,12 @@ import "./TopNews.scss";
 const TopNews: React.FC = () => {
   return (
     <div className="top-news">
-      <SectionTitle titleLabel="TOP NEWS" />
+      <SectionTitle titleLabel="TOP HEADLINES" />
+      <div className="top-news__entities">
+        <div className="top-news__entities__thumbnail--left"></div>
+        <div className="top-news__entities__thumbnail--right-top"></div>
+        <div className="top-news__entities__thumbnail--right-bottom"></div>
+      </div>
     </div>
   );
 };
