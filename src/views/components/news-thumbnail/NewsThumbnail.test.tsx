c8ab7ebcac567401947ba959dad910c64e375b1c
Stefan Misic <stefan.misic.491@gmail.com>
2021-02-18 15:32:12 +0100

    Add NewsThumbnai test

---
+++ src/views/components/news-thumbnail/NewsThumbnail.test.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+
+import NewsThumbnail, { ThumbnailType } from "./NewsThumbnail";
+
+describe("<NewsThumbnail /> test", () => {
+  it("renders expected entities", () => {
+    const thumbnailTypeProp = ThumbnailType.TopNews;
+    const thumbnailDataProp = {
+      imageUrl: "https://test-url",
+      subtitle: "Test Subtitle",
+      text: "Test Text",
+      title: "Test Title",
+    };
+
+    render(
+      <MemoryRouter>
+        <NewsThumbnail
+          thumbnailType={thumbnailTypeProp}
+          thumbnailData={thumbnailDataProp}
+        />
+      </MemoryRouter>
+    );
+
+    const thumbnailImage = screen.getByAltText(/news/i);
+    expect(thumbnailImage).toBeInTheDocument();
+
+    const expectedSubtitle = screen.getByText(/Test Subtitle/i);
+    expect(expectedSubtitle).toBeInTheDocument();
+
+    const expectedText = screen.getByText(/Test Text/i);
+    expect(expectedText).toBeInTheDocument();
+
+    const expectedTitle = screen.getByText(/Test Title/i);
+    expect(expectedTitle).toBeInTheDocument();
+  });
+});
