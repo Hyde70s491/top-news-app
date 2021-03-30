835029ae4d6e04e2195fdce5b7692e9b80973acd
Stefan Misic <stefan.misic.491@gmail.com>
2021-03-30 15:16:54 +0200

    Add searchReducer test

---
+++ src/stores/search/tests/SearchReducer.test.ts
@@ -0,0 +1,62 @@
+import searchReducer from "../SearchReducer";
+
+describe("searchReducer test", () => {
+  it("returns the initial state", () => {
+    expect(searchReducer(undefined, { type: "" })).toEqual({
+      entities: {
+        "search-headline-1": {
+          author: "",
+          content: "",
+          description: "Subtitle of search headline 1",
+          publishedAt: "",
+          source: {
+            id: "",
+            name: "",
+          },
+          title: "SEARCH HEADLINE 1",
+          url: "",
+          urlToImage: "https://picsum.photos/id/1015/600/600",
+        },
+        "search-headline-2": {
+          author: "",
+          content: "",
+          description: "Subtitle of search headline 2",
+          publishedAt: "",
+          source: {
+            id: "",
+            name: "",
+          },
+          title: "SEARCH HEADLINE 2",
+          url: "",
+          urlToImage: "https://picsum.photos/id/1015/600/600",
+        },
+        "search-headline-3": {
+          author: "",
+          content: "",
+          description: "Subtitle of search headline 3",
+          publishedAt: "",
+          source: {
+            id: "",
+            name: "",
+          },
+          title: "SEARCH HEADLINE 3",
+          url: "",
+          urlToImage: "https://picsum.photos/id/1015/600/600",
+        },
+        "search-headline-4": {
+          author: "",
+          content: "",
+          description: "Subtitle of search headline 4",
+          publishedAt: "",
+          source: {
+            id: "",
+            name: "",
+          },
+          title: "SEARCH HEADLINE 4",
+          url: "",
+          urlToImage: "https://picsum.photos/id/1015/600/600",
+        },
+      },
+    });
+  });
+});
